beb086ca2b15787f9f2ef01a762417c6aa1f19d0
Subhrajyoti <subha.ionic3@gmail.com>
2020-12-03 16:14:04 +0530

    Initial Commit

---
+++ MovieListApp/MovieListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListAppApp.swift
+//  MovieListApp
+//
+//  Created by Subhrajyoti Chakraborty on 03/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
